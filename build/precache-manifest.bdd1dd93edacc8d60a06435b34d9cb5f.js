9292dc1e098826be5538dc3c9f75c9b3bb644d1d
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-02-26 23:34:17 +0200

    add build folder

---
+++ build/precache-manifest.bdd1dd93edacc8d60a06435b34d9cb5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1981226003d2ec2eae9c",
+    "url": "/static/js/main.19812260.chunk.js"
+  },
+  {
+    "revision": "cd08e68512dc2fbc2afe",
+    "url": "/static/js/2.cd08e685.chunk.js"
+  },
+  {
+    "revision": "1981226003d2ec2eae9c",
+    "url": "/static/css/main.a0258f2f.chunk.css"
+  },
+  {
+    "revision": "fa58ac49617d15bca5dac353988c71c8",
+    "url": "/index.html"
+  }
+];
