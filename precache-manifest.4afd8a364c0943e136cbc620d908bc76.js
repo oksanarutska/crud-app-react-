56b641c9c2faf323cdc35b304cf8afbba0898ed9
oksanarutska <42785757+oksanarutska@users.noreply.github.com>
2019-03-01 22:00:51 +0200

    Updates

---
+++ precache-manifest.4afd8a364c0943e136cbc620d908bc76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67cfe778deac708bef99",
+    "url": "/crud-app-react-/static/js/runtime~main.67cfe778.js"
+  },
+  {
+    "revision": "bff0a03c7679143a74b5",
+    "url": "/crud-app-react-/static/js/main.bff0a03c.chunk.js"
+  },
+  {
+    "revision": "cd08e68512dc2fbc2afe",
+    "url": "/crud-app-react-/static/js/2.cd08e685.chunk.js"
+  },
+  {
+    "revision": "bff0a03c7679143a74b5",
+    "url": "/crud-app-react-/static/css/main.a0258f2f.chunk.css"
+  },
+  {
+    "revision": "8fe00f23d3e51e24fb5d531bd26879f9",
+    "url": "/crud-app-react-/index.html"
+  }
+];
